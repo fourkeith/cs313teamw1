c15b584268896aa154e6d4962d00392584fca7ee
fourkeith <nemimimonkey@gmail.com>
2018-05-03 09:06:13 -0500

    changed .js file

--- web/team.js
+++ web/team.js
@@ -3,9 +3,9 @@ function clicked() {
 }
 
 function changeColor() {
-    var textbox    = document.getElementById(divColor);
+    var textbox    = document.getElementById("divColor");
     
-    var div    = document.getElementById(div1);
+    var div    = document.getElementById("div1");
 
     var color  = textbox.value;
     div.style.backgroundColor = color;
