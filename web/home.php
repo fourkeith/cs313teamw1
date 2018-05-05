e51cf74093c9ce2cb7bf24e038b99dcedd5a15b2
fourkeith <nemimimonkey@gmail.com>
2018-05-05 14:59:04 -0500

    added assignments page

---
+++ web/home.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html>
+   <head>
+      <title>Keith's Page</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+   </head>
+   <body>
+      <div class="container">
+         <div class="jumbotron">
+	    <h1>Assignments</h1>
+	 </div>
+      </div>
+   </body>
+</html>
