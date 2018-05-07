49ce062ed098a886689506e3f31c8124f19860d6
fourkeith <nemimimonkey@gmail.com>
2018-05-07 15:58:16 -0500

    got rid of ordered list in nav

--- web/nav.php
+++ web/nav.php
@@ -3,10 +3,8 @@
 	          <div class="navbar-header">
 		     <a class="navbar-brand" href="/home.php">Assignments page</a>
 		  </div>
-		  <ul class="navbar-brand">
-		     <li class="active"><a href="/keith.php">Keith's Page</a></li>
-		     <li><a href="/hello.html">Prove 1</a></li>
-		     <li><a href="/shoppingCart/shoppingCart.php">Prove 3</a></li>
-		  </ul>
+		     <a href="/keith.php">Keith's Page</a>
+		     <a href="/hello.html">Prove 1</a>
+		     <a href="/shoppingCart/shoppingCart.php">Prove 3</a>
 	       </div>
 	    </nav>
