28540717d1ab90f927d88a9fc136a0ae418b9d62
fitfour <nemimimonkey@gmail.com>
2018-05-30 16:35:39 -0500

    new changes

--- web/customer.php
+++ web/customer.php
@@ -37,16 +37,16 @@
         $statement = $db->prepare("SELECT * FROM customer");
         $statement ->execute;
 
-//        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-//        {
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
         
-//        echo '<p>';
+        echo '<p>';
 //        echo     $row['customerid'] . ' ' . $row['firstname'] ' '; 
 //        echo   . $row['lastname'] . ' ' . $row['address'];
 //        echo   . $row['zip'] . ' ' . $row['phone'];
-//        echo '<p>';
+        echo '<p>';
 
-//    }
+    }
 
     ?>
 
