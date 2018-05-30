ad66239dcbd9ae41ab764811f9e91256f2d6d9b1
fitfour <nemimimonkey@gmail.com>
2018-05-30 16:21:19 -0500

    new changes

--- web/customer.php
+++ web/customer.php
@@ -1,24 +1,24 @@
 <?php 
 
-try {
-$dbUrl = getenv('DATABASE_URL');
-
-$dbopts = parse_url($dbUrl);
-
-$dbHost = $dbopts["host"];
-$dbPort = $dbopts["port"];
-$dbUser = $dbopts["user"];
-$dbPassword = $dbopts["pass"];
-$dbName = ltrim($dbopts["path"],'/');
-
-$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-}
-catch (PDOException $ex) {
-echo "Error connecting to Database. Details: $ex";
-die();
-}
-
-}
+    try {
+        $dbUrl = getenv('DATABASE_URL');
+    
+        $dbopts = parse_url($dbUrl);
+    
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"],'/');
+    
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+        $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch (PDOException $ex) {
+        echo "Error connecting to Database. Details: $ex";
+        die();
+    }
 
 ?>
 
@@ -26,29 +26,29 @@ die();
 <html>
 
 <head>
-<title>Customer Database</title>
+    <title>Customer Database</title>
 </head>
 
 <body>
-<?php  include 'nav.php'; ?>
+    <?php  include 'nav.php'; ?>
 
-<?php
+    <?php
 
-$statement = $db->prepare("SELECT * FROM customer");
-$statement ->execute;
+        $statement = $db->prepare("SELECT * FROM customer");
+        $statement ->execute;
 
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
+        
+        echo '<p>';
+        echo $row['customerid'] . ' ' . $row['firstname'] ' ' 
+           . $row['lastname'] . ' ' . $row['address']
+           . $row['zip'] . ' ' . $row['phone'];
+        echo '<p>';
 
-echo '<p>';
-echo $row['customerid'] . ' ' . $row['firstname'] ' ' 
-   . $row['lastname'] . ' ' . $row['address']
-   . $row['zip'] . ' ' . $row['phone'];
-echo '<p>';
+    }
 
-}
-
-?>
+    ?>
 
 </body>
 
