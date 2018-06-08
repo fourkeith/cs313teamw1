aeb9f95693ad7739e6258d7a46b17a9bb736c07c
fitfour <nemimimonkey@gmail.com>
2018-06-08 18:20:37 -0500

    new changes

--- web/customer.php
+++ web/customer.php
@@ -6,17 +6,17 @@
 </head>
 
 <body>
+    
+    <?php  
+        include 'nav.php';
+    ?>
+
     <form action="search.php">
         <input type="text" name="fname" value="First Name"><br />
         <input type="text" name="lname" value="Last Name"><br />
         <input type="text" name="vin"   value="Vin"><br />
         <input type="submit" value="Submit">
     </form>
-    <?php  include 'nav.php';
-
-        
-
-    ?>
 
 </body>
 
