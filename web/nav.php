dd34726de9fad16d2d272170e1e996a35672aba2
fitfour <nemimimonkey@gmail.com>
2018-06-07 21:46:50 -0500

    new changes

--- web/nav.php
+++ web/nav.php
@@ -7,7 +7,6 @@
 	     <a href="/shoppingCart/shoppingCart.php">Prove 3 - </a>
          <a href="/test/index.php">test - </a>
          <a href="/customer.php">Prove 5</a>
-         <a href="/acme/index.php">test</a>
       </div>
    </div>
 </nav>
