c18846072774dc9f5b13f53d9f350e4d4dd4dc26
fitfour <nemimimonkey@gmail.com>
2018-06-07 21:08:34 -0500

    new changes

--- web/common/footer.php
+++
@@ -1,3 +0,0 @@
-<p>&copy;TTYL, All rights reserved.</p>
-<p>All images used are believed to be in "Fair Use" Please notify the author if any are not and they will be removed.</p>
-<p>Last Updated: 1 March, 2017</p>
--- web/nav.php
+++ web/nav.php
@@ -2,12 +2,12 @@
    <div class="container-fluid">
       <div>
          <a href="/home.php">Assignments page - </a>
-	 <a href="/keith.php">Keith's Page - </a>
-	 <a href="/hello.html">Prove 1 - </a>
-	 <a href="/shoppingCart/shoppingCart.php">Prove 3 - </a>
-     <a href="/test/index.php">test - </a>
-     <a href="customer.php">Prove 5</a>
-     <a href="/acme/index.php">test</a>
+    	 <a href="/keith.php">Keith's Page - </a>
+	     <a href="/hello.html">Prove 1 - </a>
+	     <a href="/shoppingCart/shoppingCart.php">Prove 3 - </a>
+         <a href="/test/index.php">test - </a>
+         <a href="customer.php">Prove 5</a>
+         <a href="/acme/index.php">test</a>
       </div>
    </div>
 </nav>
