8a5ca15c57f4ffe38728bd1c40b9168b473be956
fitfour <nemimimonkey@gmail.com>
2018-06-09 22:52:01 -0500

    new changes

--- web/search.php
+++ web/search.php
@@ -1,7 +1,11 @@
 <?php
 
-    include 'dbaccess.php';
+    $db = include 'dbaccess.php';
 
-    
+    $query = $db->prepare('SELECT * FROM Customer WHERE firstname=:fname AND lastname=:lname');
+    $query->bindValue(':fname', $_POST['fname'], PDO::PARAM_STR);
+    $query->bindValue(':lname', $_POST['lname'], PDO::PARAM_STR);
+    $query->execute();
+    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
